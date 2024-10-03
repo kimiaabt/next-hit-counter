cc6f57302d081ee1c3a6088471bf82b7a7314007
Kimia Abtahi <kimia.abtahi@bibsent.no>
2024-10-03 17:53:10 +0200

    increase hits in database when visting page

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import {
-  readFile,
-  writeFile,
-} from '../helpers/file-helpers';
+import { readFile, writeFile } from "../helpers/file-helpers";
 
-const DATABASE_PATH = '/src/database.json';
-
-/*
-`readFile` takes 1 argument:
-• the path to the file:
-
-readFile('/path/to/file');
-
-`writeFile` takes 2 arguments:
-• The path to the file
-• The new contents for the file
-
-writeFile(
-  '/path/to/file',
-  '{ "hello": "world" }'
-);
-*/
+const DATABASE_PATH = "/src/database.json";
 
 function Home() {
+  const database = JSON.parse(readFile(DATABASE_PATH));
+  const hits = database.hits;
+  const newHits = hits + 1;
+  writeFile(DATABASE_PATH, JSON.stringify({ hits: newHits }));
+
   return (
     <main>
       <h1>Welcome!</h1>
-      <p>You are visitor number X.</p>
+      <p>You are visitor number {hits}</p>
     </main>
   );
 }
